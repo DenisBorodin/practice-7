43ce994e51adeb8342af1db02b2aafdde41333d5
Denis <94145410+DenisBorodin@users.noreply.github.com>
2021-11-21 00:54:30 +0300

    Update LogScript.java

--- src/test/java/ru/LitecartLocalhost/LogScript.java
+++ src/test/java/ru/LitecartLocalhost/LogScript.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogScript {
@@ -24,15 +25,32 @@ public class LogScript {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("remember_me")).click();
         driver.findElement(By.name("login")).click();
+
+        ArrayList<String> list = new ArrayList<>();
         List<WebElement> appMenu = driver.findElements(By.id("box-apps-menu"));
         List<WebElement> elements = appMenu.get(0).findElements(By.id("app-"));
 
-        for (int i = 0; i < elements.size(); i++) {
-            appMenu = driver.findElements(By.id("box-apps-menu"));
-            elements = appMenu.get(0).findElements(By.id("app-"));
-            elements.get(i).click();
+        for (WebElement element : elements) {
+            list.add(element.getText());
         }
-        driver.quit();
-        driver = null;
+        for (int i = 0; i < elements.size(); i++) {
+            driver.findElement((By.linkText(list.get(i)))).click();
+
+                ArrayList<String> listSecond = new ArrayList<>();
+                List<WebElement> smallElements = driver.findElements(By.xpath("//ul[@class='docs']/li"));
+
+                if (smallElements.size() > 0) {
+                    for (WebElement element1 : smallElements) {
+                        listSecond.add(element1.getText());
+                    }
+
+                    for (int x = 0; x < listSecond.size(); x++) {
+                        driver.findElement(By.linkText(listSecond.get(x))).click();
+                    }
+                }
+            }
+            driver.quit();
+            driver = null;
+        }
     }
-}
+
